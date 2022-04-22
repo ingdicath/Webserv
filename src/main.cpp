eb2b95cc5f2f99dce2244614c167d59daf2ac436
Diana Salamanca <dsalaman@student.codam.nl>
2022-04-22 12:35:56 +0200

    Upload example default.conf and html pages

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 // Created by Diana Salamanca on 20/04/2022.
 //
 
-#include <iostream>
+#include <iostream> // std::cerr
+#include <cstdlib> // macros EXIT_FAILURE, EXIT_SUCCESS
 
 int main(int argc, char **argv) {
-	if (argc != 2){
-		std::cerr << "One argument expected. Usage: ./webserv [configuration file]" << std::endl;
+	if (argc != 2) {
+		std::cerr << "One argument is expected." << std::endl;
+		std::cerr << "Usage 1: ./webserv [configuration file]" << std::endl;
+		std::cerr << "Usage 2: ./webserv [default path]" << std::endl;
 		return EXIT_FAILURE;
 	}
 	std::cout << argv[1] << " Hello, World!" << std::endl;
