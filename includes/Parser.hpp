9b3142e482bb823ea61683d7351f6cd8cc5d5f94
Diana Salamanca <dsalaman@student.codam.nl>
2022-05-11 14:50:19 +0200

    Update main

--- includes/Parser.hpp
+++ includes/Parser.hpp
@@ -2,11 +2,15 @@
 // Created by Diana catherine Salamanca leguizamon on 4/28/22.
 //
 #pragma once
+#include <string>
 
 class Parser {
 public:
 	Parser();
 	virtual ~Parser();
 
+	void getFile(const std::string &configFile);
+private:
+	std::string _configFileName;
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,40 @@
 //
 
 #include <iostream> // std::cerr
-#include <cstdlib> // macros EXIT_FAILURE, EXIT_SUCCESS
+#include <cstdlib> // for EXIT_FAILURE
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <cstring>
+
+#define DEFAULT_CONF "config_files/default.conf"
 
 int main(int argc, char **argv) {
-	Parser parser; // contains all checkers to validate config file
-	WebServer webServer; //contains methods to run webserv
+//	Parser parser; // contains all checkers to validate config file
+//	WebServer webServer; //contains methods to run webserv
 	std::string configFile; // argument
 
-
 	if (argc > 2) {
-		std::cerr << "One argument is expected." << std::endl;
-		std::cerr << "Usage 1: ./webserv [configuration file]" << std::endl;
-		std::cerr << "Usage 2: ./webserv [default path]" << std::endl;
+		std::cerr << "Invalid number of arguments." << std::endl;
+		std::cerr << "Usage: ./webserv [configuration file]" << std::endl;
 		return EXIT_FAILURE;
 	}
+	// Your program has to take a configuration file as argument, or use a default path.
+	if (argc == 1) {
+		std::cout << argv[0] << " - I'm calling the default config file!" << std::endl; // test, delete
+		configFile = DEFAULT_CONF;
+	}
+	else if (argc == 2){
+		configFile = argv[1];
+		std::cout << configFile << " - I am the config file entered by the user!" << std::endl; // test, delete
+	}
 	try {
-		// Your program has to take a configuration file as argument, or use a default path.
-		if (argc == 1) {
-			configFile = "config_files/default.conf";
-		}
-		parser.check(configFile); // validate config file
-		webServer.initialize(parser.getServerConfig()); // if is valid setup webserver with config files parameters
-		webServer.run(); //start webserv
+//		parser.check(configFile); // validate config file
+//		webServer.initialize(parser.getServerConfig()); // if is valid setup webserver with config files parameters
+//		webServer.run(); //start webserv
 	} catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
 	}
-	std::cout << argv[1] << " Hello, World!" << std::endl;
 	return EXIT_SUCCESS;
 }
