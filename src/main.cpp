bec9ed819bee30f624999a9d5d4c6bf808564553
Diana Salamanca <dsalaman@student.codam.nl>
2022-05-16 05:55:57 +0200

    Add example conf file

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,18 @@
 // Created by Diana Salamanca on 20/04/2022.
 //
 
-#include <iostream> // std::cerr
-#include <cstdlib> // for EXIT_FAILURE
-#include "settings.hpp" // for DEFAULT_CONFIG_FILE
-
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <cstring>
-
+#include <iostream>
+#include "settings.hpp"
 
 int main(int argc, char **argv) {
-//	Parser parser; // contains all checkers to validate config file
 //	WebServer webServer; //contains methods to run webserv
 	std::string configFile;
 
 	if (argc > 2) {
 		std::cerr << "Invalid number of arguments." << std::endl;
 		std::cerr << "Usage: ./webserv [configuration file]" << std::endl;
-		return EXIT_FAILURE;
+		std::exit(EXIT_FAILURE);
 	}
-	// Your program has to take a configuration file as argument, or use a default path.
 	if (argc == 1) {
 		std::cout << argv[0] << " - I'm calling the default config file!" << std::endl; // test, delete
 		configFile = DEFAULT_CONFIG_FILE;
@@ -31,7 +22,7 @@ int main(int argc, char **argv) {
 		std::cout << configFile << " - I am the config file entered by the user!" << std::endl; // test, delete
 	}
 	try {
-//		parser.check(configFile); // validate config file
+//		webServer.check(configFile); // validate config file
 //		webServer.initialize(parser.getServerConfig()); // if is valid setup webserver with config files parameters
 //		webServer.run(); //start webserv
 		std::cout << " Inside try!" << std::endl; // test, delete
