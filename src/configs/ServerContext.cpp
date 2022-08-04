0186e628b7e82e8a909b8803165304419a1ec90b
Diana Salamanca <dsalaman@student.codam.nl>
2022-08-04 15:26:31 +0200

    Delete unnecessary files, create new config files, create class ServerContext

---
+++ src/configs/ServerContext.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Diani on 04/08/2022.
+//
+
+#include "ServerContext.hpp"
---
+++ src/configs/ServerContext.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Diani on 04/08/2022.
+//
+
+#ifndef WEBSERV_SERVERCONTEXT_HPP
+#define WEBSERV_SERVERCONTEXT_HPP
+
+
+
+class ServerContext {
+
+};
+
+
+
+#endif //WEBSERV_SERVERCONTEXT_HPP
