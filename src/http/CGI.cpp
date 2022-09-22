081bda652900b40c7e4c103b04b828fe0244970d
aheister70 <74706108+aheister70@users.noreply.github.com>
2022-09-22 17:15:14 +0200

    Add POST cgi

--- src/http/CGI.cpp
+++ src/http/CGI.cpp
@@ -6,24 +6,25 @@
 /*   By: aheister <aheister@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/19 12:28:01 by aheister      #+#    #+#                 */
-/*   Updated: 2022/09/21 15:42:15 by aheister      ########   odam.nl         */
+/*   Updated: 2022/09/22 17:13:55 by aheister      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "CGI.hpp"
 #include "../webserver/Location.hpp"
 
-// TODO:    - Correct arguments execve function
-//          - Return error code if file cannot be executed or if other functions in CGI object fail
+// TODO:    - Return error code if file cannot be executed or if other functions in CGI object fail
 //              - 500 if system function fails
 //              - 403 if program can't be executed
 //          - Handle memory management of array env
+//          - Handle memory management of array args
 //          - Find out how to use the other environment variables
+//			- Handle UPLOAD?? (don't know if that is done here)
+//			- Remove type from result of cgi
 
-CGI::CGI(const e_method method, const std::string path, const std::string queryString) {
-    _method = method;
-    _file = path;
-    _queryString = queryString;
+CGI::CGI(const e_method method, const std::string path) {
+	_method = method;
+	_path = path;
 }
 
 CGI::CGI(CGI const &src) {
@@ -31,116 +32,175 @@ CGI::CGI(CGI const &src) {
 }
 
 CGI& CGI::operator=(CGI const & rhs) {
-    if (this != &rhs) {
-        _method = rhs._method;
-        _file = rhs._file;
-        _queryString = rhs._queryString;
-    }
-    return *this;
+	if (this != &rhs) {
+		_method = rhs._method;
+		_path = rhs._path;
+		_queryString = rhs._queryString;
+	}
+	return *this;
 }
 
 CGI::~CGI() {
 }
 
-int CGI::execute_cgi(char *arg, int tmp_fd, char *env[])
+int CGI::execute_cgi(char *args[], int tmp_fd, char *env[])
 {
 	dup2(tmp_fd, STDIN_FILENO);
 	close(tmp_fd);
-    // TODO: second argument can't be NULL: int execve(const char *pathname, char *const argv[],char *const envp[]);
-	execve(arg, NULL, env);
-    // if file cannot be executed than show errorpage
-    // if it fails the child must exit
-    exit (1);
+	//(void)args;
+	//(void)env;
+	execve(args[0], args, env);
+	exit (EXIT_FAILURE);
 }
-
-// which input does the cgi need to fill the environment variables:
-// If it is a POST request: CONTENT_TYPE and CONTENT_LENGTH are filled
 
 char **CGI::create_envp(void)
 {
-    std::map<std::string, std::string> env_map;
+	std::map<std::string, std::string> env_map;
 
-if (this->_method == GET)
-    env_map["QUERY_STRING"] = this->_queryString;
-//if method = POST
-    //env_map["CONTENT_TYPE"] = "";
-    //env_map["CONTENT_LENGTH"] = "";  
-    env_map["GATEWAY_INTERFACE"] = "CGI/1.1";
-    env_map["PATH_INFO"] = "";                                      //path suffix, if appended to URL after program name and a slash
-    env_map["PATH_TRANSLATED"] = "";              
-    env_map["REMOTE_ADDR"] = "";
-    env_map["REMOTE_IDENT"] = "";                                   //IP address of the client (dot-decimal)
-    env_map["REQUEST_URI"] = "";                                    //the part of URL after ? character
-    env_map["REQUEST_METHOD"] = "GET";                              //
-    env_map["SCRIPT_NAME"] = "";                                    //relative path to the program, like /cgi-bin/script.cgi
-    env_map["SCRIPT_FILENAME"] = "";                                //
-    env_map["SERVER_SOFTWARE"] = "webserv/1.0";                     //name/version of HTTP server
-    env_map["SERVER_PROTOCOL"] = "HTTP/1.1";                        //HTTP/version
-    env_map["SERVER_NAME"] = "127.0.0.1";                           //host name of the server, may be dot-decimal IP address
-    env_map["SERVER_PORT"] = "8085";                               //TCP port (decimal)
+	if (this->_method == GET)
+		env_map["QUERY_STRING"] = this->_queryString;
+	if (this->_method == POST) {
+		env_map["CONTENT_TYPE"] = this->_type;
+		env_map["CONTENT_LENGTH"] = this->_length;
+	} 
+	env_map["GATEWAY_INTERFACE"] = "CGI/1.1";
+	env_map["PATH_INFO"] = "";                                      //path suffix, if appended to URL after program name and a slash
+	env_map["PATH_TRANSLATED"] = "";              
+	env_map["REMOTE_ADDR"] = "";
+	env_map["REMOTE_IDENT"] = "";                                   //IP address of the client (dot-decimal)
+	env_map["REQUEST_URI"] = "";                                    //the part of URL after ? character
+	env_map["REQUEST_METHOD"] = "GET";                              //
+	env_map["SCRIPT_NAME"] = "";                                    //relative path to the program, like /cgi-bin/script.cgi
+	env_map["SCRIPT_FILENAME"] = "";                                //
+	env_map["SERVER_SOFTWARE"] = "";                                //name/version of HTTP server
+	env_map["SERVER_PROTOCOL"] = "HTTP/1.1";                        //HTTP/version
+	env_map["SERVER_NAME"] = "127.0.0.1";                           //host name of the server, may be dot-decimal IP address
+	env_map["SERVER_PORT"] = "8085";                                //TCP port (decimal)
 
-    return (convert_map_to_array(env_map));
+	return (convert_map_to_array(env_map));
+}
+
+char **CGI::create_args(void) {
+	char **args;
+	int size;
+
+	if (_method == POST)
+		size = 3;
+	else	
+		size = 2;
+	args = (char **)malloc(size * sizeof(char *));
+	args[0] = const_cast<char*>(_path.c_str());
+	if (_method == POST) {
+		args[1] = const_cast<char*>(_requestBody.c_str());
+		args[2] = NULL;
+	}
+	else
+		args[1] = NULL;
+	return(args);
 }
 
 char **CGI::convert_map_to_array(std::map<std::string, std::string> env_map)
 {
-    int i;
-    char **envp;
-    std::string env_var;
+	int i;
+	char **envp;
+	std::string env_var;
 
-    envp = NULL;
-    envp = (char **)malloc((env_map.size() + 1) * sizeof(char *));
-    i = 0;
-    for (std::map<std::string, std::string>::const_iterator it = env_map.begin(); it != env_map.end(); ++it)
-    {
-        envp[i] = strdup((it->first + "=" + it->second).c_str());
-		if (envp[i] == NULL)
-			throw("error happened");
+	envp = NULL;
+	envp = (char **)malloc((env_map.size() + 1) * sizeof(char *));
+	i = 0;
+	for (std::map<std::string, std::string>::const_iterator it = env_map.begin(); it != env_map.end(); ++it)
+	{
+		envp[i] = strdup((it->first + "=" + it->second).c_str());
+		// if (envp[i] == NULL)
+		// 	throw("error happened");
 		i++;
 	}
 	envp[i] = NULL;
-    return (envp);
+	return (envp);
+}
+
+std::string	CGI::execute_GET(std::string queryString) {
+	std::string body;
+
+	setQueryString(queryString);
+	body = execute();
+	return (body);
+}
+
+std::string	CGI::execute_POST(std::string type, std::string requestBody) {
+	std::string body;
+
+	setType(type);
+	setLength(requestBody.size());
+	setRequestBody(requestBody);
+	body = execute();
+	return (body);
 }
 
 std::string CGI::execute(void) {
-    int         ret;
-    int         pid;
+	int         ret;
+	int         pid;
 	int         tmp_fd;
-    int         fd[2];
-    std::string body;
-    char        **envp;
+	int         fd[2];
+	std::string body;
+	char        **envp;
+	char		**args;
 
-    pid = 0;
+	pid = 0;
+	envp = create_envp();
+	// error if envp = NULL
+	args = create_args();
+	// error if args = NULL
 	tmp_fd = dup(STDIN_FILENO);
-    pipe(fd);
-    pid = fork();
-    if (pid == 0)
-    {
-        dup2(fd[1], STDOUT_FILENO);
+	// error if dup fails
+	pipe(fd);
+	// error if pipe fails
+	pid = fork();
+	// error if fork fails
+	if (pid == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		// error if dup fails
 		close(fd[0]);
 		close(fd[1]);
-        envp = create_envp();
-        if (envp)
-            execute_cgi(const_cast<char*>(_file.c_str()), tmp_fd, envp);
-        //else
-            //HIER NOG FOUTMELDING
-    }
-    else
-    {
-        char	buffer[CGI_BUFSIZE] = {0};
+		execute_cgi(args, tmp_fd, envp);
+	}
+	else
+	{
+		char	buffer[CGI_BUFSIZE] = {0};
 
 		ret = 1;
-        while(waitpid(-1, NULL, WUNTRACED) != -1)
-					;
-        close(fd[1]);
-        close(tmp_fd);
+		while(waitpid(-1, NULL, WUNTRACED) != -1)
+			;
+		close(fd[1]);
+		close(tmp_fd);
 		while (ret > 0)
 		{
 			memset(buffer, 0, CGI_BUFSIZE);
 			ret = read(fd[0], buffer, CGI_BUFSIZE - 1);
+			// error if read fails??
+			// think about checking fds for select here? Possible?
 			body += buffer;
 		}
-        close(fd[0]);
-    }
-    return (body);
+		close(fd[0]);
+	}
+	return (body);
+}
+
+// setters
+
+void    CGI::setQueryString(std::string queryString) {
+	this->_queryString = queryString;
+}
+
+void    CGI::setType(std::string type) {
+	this->_type = type;
+}
+
+void    CGI::setLength(long length) {
+	this->_length = length;
+}
+
+void    CGI::setRequestBody(std::string requestBody) {
+	this->_requestBody = requestBody;
 }
